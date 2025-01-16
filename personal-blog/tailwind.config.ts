10ca1d10078cda3e32fd570bc70292c9cc7e4c89
Bagas Noor <19623186@std.stei.itb.ac.id>
2025-01-16 21:07:33 +0700

    fix: replace CDN

---
+++ personal-blog/tailwind.config.ts
@@ -0,0 +1,23 @@
+import { Inter, Lora } from "next/font/google";
+import { title } from "process";
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: "Inter, 'sans-serif'",
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
