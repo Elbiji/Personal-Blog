ce4b687298928c40251adbfc7afb88be45b2255a
Bagas Noor <19623186@std.stei.itb.ac.id>
2025-07-02 10:22:11 +0700

    style: added underline animation for the social bar

--- personal-blog/app/page.tsx
+++ personal-blog/app/page.tsx
@@ -213,16 +213,18 @@ export default function Home() {
         <footer className="pb-10 pt-10">
           <div className="inset-0  mt-4 p-[1.4px] rounded-xl shadow-xl bg-gradient-to-tr from-neutral-300 via-neutral-200 to-neutral-100">
             <div className="p-[4px] flex h-full w-full rounded-[0.65rem] bg-white justify-between items-center">
-              <div className="flex items-center gap-1">
-                  <div className="group border-solid border-2 p-[8px] rounded-lg hover:shadow-xl transition-all duration-500 cursor-pointer shadow-sm group  bg-gradient-to-br from-white via-white to-neutral-100">
-                    <a href="https://github.com/Elbiji" className="hover:text-emerald-700 duration-[3000]">
+              <div className="group flex items-center gap-1">
+                  <div className="border-solid border-2 p-[8px] rounded-lg hover:shadow-xl transition-all duration-[1200ms] cursor-pointer shadow-sm group bg-gradient-to-tr from-white via-white to-neutral-200 group-hover:rotate-12">
+                    <a className="hover:text-emerald-700 duration-[3000]">
                       <svg className="duration-300  fill-neutral-400  group-hover:rotate-12 group-hover:animate-pulse group-hover:fill-neutral- group-hover:scale-150 transition-all" width="16" height="16" viewBox="0 0 25 25">
                         <path d="M3.5 21V16M3.5 6V1M1 3.5H6M1 18.5H6M12 2L10.2658 6.50886C9.98381 7.24209 9.84281 7.60871 9.62353 7.91709C9.42919 8.1904 9.1904 8.42919 8.91709 8.62353C8.60871 8.8428 8.24209 8.98381 7.50886 9.26582L3 11L7.50886 12.7342C8.24209 13.0162 8.60871 13.1572 8.91709 13.3765C9.1904 13.5708 9.42919 13.8096 9.62353 14.0829C9.84281 14.3913 9.98381 14.7579 10.2658 15.4911L12 20L13.7342 15.4911C14.0162 14.7579 14.1572 14.3913 14.3765 14.0829C14.5708 13.8096 14.8096 13.5708 15.0829 13.3765C15.3913 13.1572 15.7579 13.0162 16.4911 12.7342L21 11L16.4911 9.26582C15.7579 8.98381 15.3913 8.8428 15.0829 8.62353C14.8096 8.42919 14.5708 8.1904 14.3765 7.91709C14.1572 7.60871 14.0162 7.24209 13.7342 6.50886L12 2Z"/>
                       </svg>
                     </a>
                   </div>
-                <p className="font-Inter text-xs font-bold text-neutral-500">
-                  Socials 
+                <p className="font-Inter text-xs text-neutral-500 transition duration-300">
+                  <span className="font-bold ">Socials. </span>
+                   my works.
+                   <span className="block max-w-0 group-hover:max-w-full transition-all duration-1000 h-[1px] bg-neutral-500"></span>
                 </p>
               </div>
               <div className="flex items-center justify-start">
